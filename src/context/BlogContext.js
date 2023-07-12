f96f32e605b4c5a55407286d109f9e355e665b60
Chris Kim <hjchristopher.dev@gmail.com>
2023-07-11 22:34:43 -0700

    Automate context and add in delete blog functionality

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,18 +1,36 @@
-import React, {useState} from "react";
+import createDataContext from "./createDataContext";
 
-const BlogContext = React.createContext();
+const blogReducer = (state, action) => {
+    switch (action.type) {
+        case 'delete_blogpost':
+            return state.filter((blogPost) => blogPost.id !== action.payload);
+        case 'add_blogpost':
+            return [
+                ...state, 
+                { 
+                    id: Math.floor(Math.random() * 99999), 
+                    title: `Blog Post #${state.length+1}` 
+                }
+            ]
+        default:
+            return state;
+    }
+}; 
 
-export const BlogProvider = ({children}) => {
-
-    const [blogPosts, setBlogPosts] = useState([]);
-
-    const addBlogPost = () => {
-        setBlogPosts([...blogPosts, {title: `Blog Post #${blogPosts.length+1}`}])
+const addBlogPost = dispatch => {
+    return () => {
+        dispatch({ type: 'add_blogpost' });
     };
-
-    return <BlogContext.Provider value={{data: blogPosts, addBlogPost}}>
-        {children}
-    </BlogContext.Provider>
 };
 
-export default BlogContext;
+const deleteBlogPost = dispatch => {
+    return (id) => {
+        dispatch({ type: 'delete_blogpost', payload: id });
+    }
+};
+
+export const { Context, Provider } = createDataContext(
+    blogReducer, 
+    { addBlogPost, deleteBlogPost }, 
+    []    
+);
