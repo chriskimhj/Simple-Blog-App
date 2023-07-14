5682705573cf69855ca5db1dc9a6113b043267e7
Chris Kim <hjchristopher.dev@gmail.com>
2023-07-13 23:55:27 -0700

    Extract blog post logic to separate component

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -3,12 +3,16 @@ import { StyleSheet } from 'react-native';
 import { Context } from "../context/BlogContext";
 import BlogPostForm from "../components/BlogPostForm";
 
-const CreateScreen = ({ navigation }) => {
-    const { addBlogPost } = useContext(Context);
+const EditScreen = ({ navigation }) => {
+    const { state } = useContext(Context);
+
+    const blogPost = state.find(
+        blogPost => blogPost.id === navigation.getParam('id')
+    );
 
     return <BlogPostForm />
 };
 
 const styles = StyleSheet.create({});
 
-export default CreateScreen;
+export default EditScreen;
